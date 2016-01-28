b49e52bd7569d0e53fd9bd1d9608004dc58f2c14
Paniez <ppaykari@Paniezs-MacBook-Pro.local>
2016-01-28 14:41:28 +0000

    testing conflicts

--- python/circle.py
+++ python/circle.py
@@ -1,10 +1,10 @@
 ''' Functions from http://math2.org/math/geometry/circles.htm '''
-
+ import numpy as np 
 def circle_area(radius=1):
     return 3.14 * radius ** 2
 	return math.pi
 def circle_circunference(radius=1):
-    return 2*3.14*radius
+    return 2*np.pi*radius
 
 def circle_length_arc(radius, angle):
     pass
